44ae102653649fefda14461df3bdc5db285ea9b0
nicks9188 <nicks9188@gmail.com>
2019-03-26 00:32:17 +0530

    adding code

---
+++ HelloC.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : HelloC.c
+ Author      : Nicketa K
+ Version     :
+ Copyright   : 2019 All Rights Reserved
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
