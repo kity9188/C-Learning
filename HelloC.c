d25679256b51b2a915d6f57c42a17d31057c671d
nicks9188 <nicks9188@gmail.com>
2019-03-26 20:51:55 +0530

    placeholder

--- HelloC.c
+++ HelloC.c
@@ -1,22 +1,12 @@
-/*
- ============================================================================
- Name        : HelloC.c
- Author      : Nicketa K
- Version     :
- Copyright   : 2019 All Rights Reserved
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	printf("!!!Hello World!!!\n"); /* prints !!!Hello World!!! */
-	printf("%d",32); /* %d is a placeholder which holds a place for integer. d stands for integer */
-	printf("\n");
-	printf("19 + 31 is : %d", 19+31);
-	printf("\n");
-	printf("%f", 32.9); /* f is a placeholder for float/decimal value */
+int main() {
+	printf("Hello from C");
 	return EXIT_SUCCESS;
 }
+
+//int main(void) {
+//	printf("!!!Hello World!!!\n"); /* prints !!!Hello World!!! */
+//	return EXIT_SUCCESS;
+//}
