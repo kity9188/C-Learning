de4e46de7e27c8042e046d8fa33108089b4cced6
nicks9188 <nicks9188@gmail.com>
2019-03-26 20:54:01 +0530

    initial

---
+++ placeholder.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Printing Numbers:\n");
+	printf("The number is =%d",32);
+	printf("\n");
+	printf("19 + 31 is : %d", 19+31);
+	printf("\n");
+	printf("%f", 32.9);
+	return EXIT_SUCCESS;
+}
